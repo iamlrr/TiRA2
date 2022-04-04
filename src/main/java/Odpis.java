0eca2c4d66703735dd216860b52d6759adf447c0
iamlrr <iamlrr@protonmail.ch>
2022-04-04 12:08:54 -0700

    Added class Odpis

--- src/main/java/Odpis.java
+++ src/main/java/Odpis.java
@@ -2,7 +2,28 @@ import java.util.Date;
 
 public class Odpis {
     Date date;
+    Uczen uczen;
 
-
+    public Odpis(Date date, Uczen uczen) {
+        this.date = date;
+        this.uczen = uczen;
     }
+
+    public Date getDate() {
+        return date;
+    }
+
+
+    public Uczen getUczen() {
+        return uczen;
+    }
+
+    @Override
+    public String toString() {
+        return "Odpis{" +
+                "date=" + date +
+                ", uczen=" + uczen +
+                '}';
+    }
+}
 
